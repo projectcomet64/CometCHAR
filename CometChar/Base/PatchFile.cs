822c0547e8d2a9f315893a3f68a0533ede02fc80
GlitchyPSI <supermglitchy3@gmail.com>
2020-04-07 00:19:24 -0400

    Add PInfo structs

--- CometChar/Base/PatchFile.cs
+++ CometChar/Base/PatchFile.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CometChar
 {
-    static class PatchFile
+    public static class Patch
     {
+
+
+
     }
 }
---
+++ CometChar/Base/Structs/PatchInformation.cs
@@ -0,0 +1,18 @@
+﻿namespace CometChar.Structs
+{
+    /// <summary>
+    /// Information about the patch.
+    /// </summary>
+    public struct PatchInformation
+    {
+        public int versionMajor;
+        public int versionMinor;
+        public ushort Features;
+        public uint compGeoLayoutLength;
+        public uint compSegment04Length;
+        public uint CompressedCRC;
+        public uint Segment04Length;
+        public uint GeoLayoutLength;
+        public uint GeoLayoutSegOffset;
+    }
+}
